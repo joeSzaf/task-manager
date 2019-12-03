0903c76d97a9247e96f12a988ac931385808a964
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-12-02 20:58:54 -0500

    add timestamps to user and tasks

--- src/index.js
+++ src/index.js
@@ -26,3 +26,18 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log("Server is up on " + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+  // const task = await Task.findById('5dbcdf354a23224da952e6c5')
+  // await task.populate('owner').execPopulate()
+  // console.log(task.owner.name)
+
+  const user = await User.findById('5dbcdebcdf550e4d4ec219d0')
+  await user.populate('tasks').execPopulate() 
+  console.log(user.tasks)
+}
+
+main()
